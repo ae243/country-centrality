0600d2ad9511276328a01394beefb922e1ed8813
ae243 <annee@cs.princeton.edu>
2015-11-17 17:56:19 -0500

    added partially functional measurement tool

--- code/measurement_tool/run_measurement.py
+++ code/measurement_tool/run_measurement.py
@@ -18,26 +18,28 @@ f = open('log.txt', 'w')
 f.close()
 
 for domain in open('domains.txt', 'r'):
+    
     # 1) curl the list of domains in domains.txt -- currently uses planetlab
     # TODO: have RIPE Atlas send the HTTP or HTTPS request
     page = util.curl(domain.strip())
     if page == "":
         # this is an error on curling the domain
         continue
-
+    
     # 2) extract the urls from the domains's web page
-#    urls = util.extract_urls(page)
-#    urls.append(domain.strip())
+    urls = util.extract_urls(page)
+    urls.append(domain.strip())
 
     # 3) run DNS measurements for all domains in the web page, as well as the original domain
-#    ips = util.run_DNS_measurements(urls)
+    ips = util.run_DNS_measurements(urls)
     ips = []
+
     # 4) run traceroute measurements for all DNS responses (from corresponding probes)
-#    traceroutes = util.run_traceroute_measurements(ips)
+    traceroutes = util.run_traceroute_measurements(ips)
 
     # 5) map traceroutes to country-level paths
-#    country_paths = util.get_country_level_paths(traceroutes)
+    country_paths = util.get_country_level_paths(traceroutes)
 
     # 6) store traceroutes and country-level paths in some sort of structure/database/location
-#    util.store(traceroutes)
-#    util.store(country_paths)
+    util.store(traceroutes)
+    util.store(country_paths)
