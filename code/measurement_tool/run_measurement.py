3a93ba46b0fa816bd529d3bb2fe37f0ce1234676
ae243 <annee@cs.princeton.edu>
2015-12-07 14:36:07 -0500

    updated working measurement tool

--- code/measurement_tool/run_measurement.py
+++ code/measurement_tool/run_measurement.py
@@ -13,33 +13,62 @@
 
 import sys
 import util
+import time
 
 f = open('log.txt', 'w')
 f.close()
 
-for domain in open('domains.txt', 'r'):
+targets = []
+total_urls = []
+domains_file = open('domains.txt', 'r')
+for domain in domains_file:
     
     # 1) curl the list of domains in domains.txt -- currently uses planetlab
-    # TODO: have RIPE Atlas send the HTTP or HTTPS request
     page = util.curl(domain.strip())
-    if page == "":
-        # this is an error on curling the domain
-        continue
     
     # 2) extract the urls from the domains's web page
-    urls = util.extract_urls(page)
-    urls.append(domain.strip())
+    urls = [domain]
+    urls.extend(util.extract_urls(page))
+    total_urls.append(urls)
+    targets.extend(urls)
 
-    # 3) run DNS measurements for all domains in the web page, as well as the original domain
-    ips = util.run_DNS_measurements(urls)
-    ips = []
+# 3) store domains and urls
+util.store_domains(total_urls)
+domains_file.close()
 
-    # 4) run traceroute measurements for all DNS responses (from corresponding probes)
-    traceroutes = util.run_traceroute_measurements(ips)
+# 4) Modify targets to include 'correct' urls
+targets = util.clean_targets(targets)
 
-    # 5) map traceroutes to country-level paths
-    country_paths = util.get_country_level_paths(traceroutes)
+print "TARGETS:"
+print targets
+print "\n"
 
-    # 6) store traceroutes and country-level paths in some sort of structure/database/location
-    util.store(traceroutes)
-    util.store(country_paths)
+# 4) run DNS measurements for all domains in the web page, as well as the original domain
+dns_measurement_ids = util.run_DNS_measurements(targets)
+print "DNS MEASURMENT IDS:"
+print dns_measurement_ids
+print "\n"
+
+time.sleep(600) # wait for 20 minutes for DNS measurements to end - is there a better way to do this?
+ips = util.analyze_DNS_measurements(dns_measurement_ids)
+
+print "IP ADDRESSES:"
+print ips
+print "\n"
+
+# 5) run traceroute measurements for all DNS responses (from corresponding probes)
+traceroute_measurement_ids = util.run_traceroute_measurements(ips, 'ICMP')
+traceroute_measurement_ids.extend(util.run_traceroute_measurements(ips, 'TCP'))
+time.sleep(600) # wait for 30 minutes for traceroute measurements to end - is there a better way to do this?
+traceroutes = util.analyze_traceroute_measurements(traceroute_measurement_ids)
+
+print "TRACE MEASUREMENT IDS:"
+print traceroute_measurement_ids
+print "\n"
+
+print "TRACEROUTES:"
+print traceroutes
+print "\n"
+
+# 6) store traceroutes and domains (with urls)
+util.store_traces(traceroutes, sys.argv[1])
