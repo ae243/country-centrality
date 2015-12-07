f6f68f757344a982573866ffba74a36281e6f4bb
ae243 <annee@cs.princeton.edu>
2015-12-07 14:36:07 -0500

    updated working measurement tool

--- code/measurement_tool/util.py
+++ code/measurement_tool/util.py
@@ -6,9 +6,7 @@ import json
 import requests
 import dns.message
 import re
-from geopy.geocoders import Nominatim
-from geopy.distance import vincenty
-from geopy.exc import GeocoderTimedOut
+import datetime
 
 def traceroute_parse(data):
     trace_list = []
@@ -31,7 +29,8 @@ def dns_parse(data):
                 for dnsm in dnsmessage.split(";"):
                     if 'ANSWER' in dnsm:
                         for l in dnsm.split("\n")[1:-1]:
-                            ip_list.append(l.split(" ")[-1])
+                            if l.split(" ")[-2] == 'A':
+                                ip_list.append(l.split(" ")[-1])
             else:
                 print "dns error"
     return ip_list
@@ -53,178 +52,97 @@ def extract_urls(page):
         domains.append('http' + u)
     return domains
 
-# TODO: update to use probes.txt
-def run_DNS_measurements(urls):
-    ip_list = []
-    for u in urls:
-        # Send this command (but fix quotations first and add <DOMAIN>)
-        # Also, implement some sort of "wait-until-measurement-is-done" before curling the response
-        #s = 'curl -H "Content-Type: application/json" -H "Accept: application/json" -X POST -d '
-        #s += "'{ "
-        #s += '"definitions": [ { "query_class": "IN", "query_type": "A", "query_argument": "' + u + '", "description": "DNS measurement to ' + u  + '", "type": "dns", "af": 4, "use_probe_resolver": true, "is_oneoff": true } ], "probes": [ { "requested": 2, "type": "probes", "value": "3363,15800" } ] }'
-        #s += "' https://atlas.ripe.net/api/v1/measurement/?key=fc3f4123-89ce-4e9e-9c01-425182696abd"
-        #response = subprocess.check_output(s, stderr = subprocess.STDOUT, shell=True)
+def clean_targets(targets):
+    clean = []
+    for t in targets:
+        if t[0:4] == 'http':
+            t = t[8:]
+        if t[0:5] == 'https':
+            t = t[9:]
+        clean.append(t.split("/")[0])
+    return clean
 
-        #temp = response.split(":")[-1]
-        #measurement_list = temp[1:-2].split(",")
-        measurement_list = ['2850269']
-        for m in measurement_list:
-            x = requests.get("https://atlas.ripe.net/api/v1/measurement/" + m  +  "/result/",)
-            data = json.loads(x.text)
-            ip_list.extend(dns_parse(data))
+def get_probes():
+    f = open('probes.txt', 'r')
+    s = ''
+    for line in f:
+        s += line.strip() + ','
+    f.close()
+    return s[:-1]
+
+def get_probes_count():
+    f = open('probes.txt', 'r')
+    count = 0
+    for line in f:
+        count += 1
+    f.close()
+    return count
+
+def run_DNS_measurements(urls):
+    measurement_list = []
+    for u in urls:
+        # Also, implement some sort of "wait-until-measurement-is-done" before curling the response
+        s = 'curl -H "Content-Type: application/json" -H "Accept: application/json" -X POST -d '
+        s += "'{ "
+        s += '"definitions": [ { "query_class": "IN", "query_type": "A", "query_argument": "' + u.strip() + '", "description": "DNS measurement to ' + u.strip()  + '", "type": "dns", "af": 4, "use_probe_resolver": true, "is_oneoff": true } ], "probes": [ { "requested": ' + str(get_probes_count()) + ', "type": "probes", "value": "' + get_probes() + '" } ] }'
+        s += "' https://atlas.ripe.net/api/v1/measurement/?key=fc3f4123-89ce-4e9e-9c01-425182696abd"
+        print s
+
+        response = subprocess.check_output(s, stderr = subprocess.STDOUT, shell=True)
+
+        print response
+
+        temp = response.split(":")[-1]
+        measurement_list.extend(temp[1:-2].split(","))
+    return measurement_list
+
+def analyze_DNS_measurements(id_list):
+    ip_list = []
+    for measurement in id_list:
+        x = requests.get("https://atlas.ripe.net/api/v1/measurement/" + str(measurement) +  "/result/",)
+        data = json.loads(x.text)
+        ip_list.extend(dns_parse(data))
     return list(set(ip_list))
 
-# TODO: update to use probes.txt
-def run_traceroute_measurements(ips):
-    traceroute_list = []
-    ips.append('216.58.222.41')
+def run_traceroute_measurements(ips, type):
+    measurement_list = []
     for ip in ips:
-        # Send this command (but fix quotations first and add <IPADDRESS>)
         # Also, implement some sort of "wait-until-measurement-is-done" before curling the response
-        #s = 'curl -H "Content-Type: application/json" -H "Accept: application/json" -X POST -d '
-        #s += "'{ "
-        #s += '"definitions": [ { "target": "' + ip + '", "protocol": "ICMP", "description": "Traceroute measurement to ' + ip + '", "type": "traceroute", "af": 4, "is_oneoff": true } ], "probes": [ { "requested": 2, "type": "probes", "value": "3363,15800" } ] }'
-        #s += "' https://atlas.ripe.net/api/v1/measurement/?key=fc3f4123-89ce-4e9e-9c01-425182696abd"
-        #response = subprocess.check_output(s, stderr = subprocess.STDOUT, shell=True)
+        s = 'curl -H "Content-Type: application/json" -H "Accept: application/json" -X POST -d '
+        s += "'{ "
+        s += '"definitions": [ { "target": "' + ip.strip() + '", "protocol": "' + type + '", "description": "Traceroute measurement to ' + ip.strip() + '", "type": "traceroute", "af": 4, "is_oneoff": true } ], "probes": [ { "requested": ' + str(get_probes_count()) + ', "type": "probes", "value": "' + get_probes() + '" } ] }'
+        s += "' https://atlas.ripe.net/api/v1/measurement/?key=fc3f4123-89ce-4e9e-9c01-425182696abd"
+        response = subprocess.check_output(s, stderr = subprocess.STDOUT, shell=True)
 
-        #temp = response.split(":")[-1]
-        #measurement_list = temp[1:-2].split(",")
-        measurement_list = ['2853537']
-        for m in measurement_list:
-            x = requests.get("https://atlas.ripe.net/api/v1/measurement/" + m  +  "/result/",)
-            data = json.loads(x.text)
-            traceroute_list.extend(traceroute_parse(data))
+        temp = response.split(":")[-1]
+        measurement_list.extend(temp[1:-2].split(","))
+    return measurement_list
+
+def analyze_traceroute_measurements(id_list):
+    traceroute_list = []
+    for measurement in id_list:
+        x = requests.get("https://atlas.ripe.net/api/v1/measurement/" + str(measurement)  +  "/result/",)
+        data = json.loads(x.text)
+        traceroute_list.extend(traceroute_parse(data))
     return traceroute_list
 
-def get_geo_distances(countries):
-    geolocator = Nominatim()
-    br = geolocator.geocode('BR', timeout=10)
-    br_coord = (br.latitude, br.longitude)
-    dist_map = {}
-    for c in countries:
-        print c
-        location1 = geolocator.geocode(c, timeout=10)
-        loc1_coord = (location1.latitude, location1.longitude)
-        dist_map[c] = float(vincenty(loc1_coord, br_coord).miles)
-    return dist_map
-
-def geo_dist(x, y):
-    geolocator = Nominatim()
-    try:
-        location1 = geolocator.geocode(x, timeout=10)
-        location2 = geolocator.geocode(y, timeout=10)
-        loc1 = (location1.latitude, location1.longitude)
-        loc2 = (location2.latitude, location2.longitude)
-        return vincenty(loc1, loc2).miles
-    except GeocoderTimedOut as e:
-        print("Error: geocode failed on input %s,%s"%(x,y))
-        return 0
-
-# naive geolocation tool based on RTT
-def get_country_level_paths(traceroutes):
-    f = open('anomalous_ips.txt', 'a')
-
-    # 1. Look up all IP addresses in Team Cymru
-    ips = []
-    for trace in traceroutes:
-        for t in trace:
-            ip = t.split(" ")[1].strip()
-            if ip != '***':
-                ips.append(ip)
-    ip_list = list(set(ips))
-
-    f3 = open('netcat_temp.txt', 'w')
-    f3.write('begin\n')
-    f3.write('verbose\n')
-    for ip in ip_list:
-        f3.write(ip + '\n')
-    f3.write('end\n')
-    f3.close()
-
-    command = ('netcat whois.cymru.com 43 < netcat_temp.txt | sort -n > list02')
-    r = subprocess.Popen(command, shell=True)
-    r.wait()
-
-    ip_cc_map = {}
-    c_list = []
-    f_temp = open('list02', 'r')
-    for line in f_temp:
-        if line.split("|")[0].strip().isdigit() or line.split("|")[0].strip() == 'NA':
-            if line.split("|")[1].strip() in ip_cc_map:
-                ip_cc_map[line.split("|")[1].strip()].append(line.split("|")[3].strip())
-            else:
-                ip_cc_map[line.split("|")[1].strip()] = [line.split("|")[3].strip()]
-
-            if line.split("|")[3].strip() != '':
-                c_list.append(line.split("|")[3].strip())
-    f_temp.close()
-
-    countries = list(set(c_list))
-    dist_map = get_geo_distances(countries)
-
-    country_paths = []
-    for trace in traceroutes:
-        c_path = []
-        # 1. Look up countries in map for whole trace
-        for t in trace:
-            if t.split(" ")[1].strip() != '***':
-                cc = ip_cc_map[t.split(" ")[1].strip()][0]
-                c_path.append(cc)
-            else:
-                c_path.append('***')
-
-        domestic_rtt = ''
-        for i in range(0, len(trace)):
-            if c_path[i].strip() == 'BR':
-                if trace[i].split(" ")[2].strip() != '':
-                    domestic_rtt = trace[i].split(" ")[2].strip()
-                    break
-
-        if domestic_rtt != '':
-            # 2. Check if RTT matches country (sequence ABA with no significant change in RTT)
-            new_path = []
-            for i in range(0,len(trace)):
-                new_line = ''
-                if i > 0:
-                    pass_check = True
-                    if trace[i].split(" ")[1] == '***':
-                        new_line = trace[i].strip()
-                    elif c_path[i] == '':
-                        new_line = trace[i].strip()
-                    elif (float(float(trace[i].split(" ")[2].strip()) - float(domestic_rtt)) > 60.0) and (float(dist_map[c_path[i]]) < 900.0): 
-                        # rtt_diff large and geo_dist small
-                        pass_check = False
-                        # check country sequence (either there must be a cc change at this hop or the next hop)
-                        if c_path[i] != c_path[i-1]:
-                            pass_check = True
-                        if i < len(trace) - 1:
-                            if c_path[i] != c_path[i + 1]:
-                                pass_check = True
-                    elif (float(float(trace[i].split(" ")[2].strip()) - float(domestic_rtt)) < 60.0) and (float(dist_map[c_path[i]]) > 900.0):
-                        # rtt_diff small and geo_dist large (should be domestic)
-                        c_path[i] = 'BR'
-                    if not pass_check:
-                        f.write(trace[i].split(" ")[1].strip() + '\n')
-                        new_line = trace[i]
-                    else:
-                        new_line = trace[i].strip() + ' ' + c_path[i]
-                else:
-                    new_line = trace[i].strip() + ' ' + c_path[i]
-                new_path.append(new_line)
-            country_paths.append(new_path)
-        else:
-            f_non_brazil = open('non_brazil_traces.txt', 'a')
-            for tr in trace:
-                f_non_brazil.write(tr + '\n')
-            f_non_brazil.close()
-    f.close()
-    return country_paths
-
 # store each traceroute file with a measurement run ID
-def store(data, measurementID):
-    f = open('run_' + str(measurementID), 'w')
+def store_traces(data, name):
+    fmt='%Y-%m-%d-%H-%M-%S_{fname}'
+    fname = name
+    f = open(datetime.datetime.now().strftime(fmt).format(fname=fname), 'w')
     for d in data:
         for d2 in d:
             f.write(d2 + '\n')
-        f.write('*\n')
+    f.close()
+
+def store_domains(data):
+    fmt='%Y-%m-%d-%H-%M-%S_{fname}'
+    fname = 'domains'
+    f = open(datetime.datetime.now().strftime(fmt).format(fname=fname), 'w')
+    for d in data:
+        s = ''
+        for d2 in d:
+            s += d2.strip() + ','
+        f.write(s.strip() + '\n')
     f.close()
