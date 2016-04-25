c036c4370da42471c88c279bf93f64feaea7f366
ae243 <annee@cs.princeton.edu>
2016-04-25 10:36:33 -0400

    removed spaulding copy

--- measurement_tool_spaulding/dnspython-1.12.0/tests/utest.py
+++
@@ -1,8 +0,0 @@
-import os.path
-import sys
-import unittest
-
-if __name__ == '__main__':
-    sys.path.insert(0, os.path.realpath('..'))
-    suites = unittest.defaultTestLoader.discover('.')
-    unittest.TextTestRunner(verbosity=2).run(suites)
