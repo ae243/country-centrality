60e94913ce7c1c1b6c66072c8083da9beb4f23e4
ae243 <annee@cs.princeton.edu>
2016-05-18 20:52:34 -0400

    paper updates

--- graphs/domain_histogram.py
+++ graphs/domain_histogram.py
@@ -1,5 +1,7 @@
 import sys
 import matplotlib.pyplot as plt
+import numpy as np
+from pylab import *
 
 f = open(sys.argv[1], 'r')
 domain_list = []
@@ -10,9 +12,26 @@ for line in f:
     domain_list.append(len(unique_items))
 f.close()
 
-n, bins, patches = plt.hist(domain_list, 50, facecolor='green')
-plt.xlabel('# of Subsequent Requests')
-plt.ylabel('# of Initial Domain Requests')
-plt.title('Histogram of Subsequent Requests made for Initial Domain Request')
-plt.axis([0, 200, 0, 60])
+#n, bins, patches = plt.hist(domain_list, 50, facecolor='green')
+d_map = {}
+for d in domain_list:
+    if d in d_map:
+        d_map[d] += 1
+    else:
+        d_map[d] = 1
+
+keylist = d_map.keys()
+keylist.sort()
+final_map = {}
+for key in keylist:
+    final_map[key] = d_map[key]
+
+x = len(final_map)
+cy = np.cumsum(d_map.values())
+plt.plot(x,cy)
 plt.show()
+#plt.xlabel('# of Subsequent Requests')
+#plt.ylabel('# of Initial Domain Requests')
+#plt.title('Histogram of Subsequent Requests made for Initial Domain Request')
+#plt.axis([0, 200, 0, 60])
+#plt.show()
