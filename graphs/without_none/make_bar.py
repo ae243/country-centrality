9b2082a3b887f451505ae4af37a3bc1f3794b1df
ae243 <annee@cs.princeton.edu>
2016-05-21 18:01:29 -0400

    added more of Jen's comments

--- graphs/without_none/make_bar.py
+++ graphs/without_none/make_bar.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt; plt.rcdefaults()
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 import sys
 
 f = open(sys.argv[1], 'r')
@@ -34,21 +33,6 @@ plt.xticks(y_pos, objects)
 locs, labels = plt.xticks()
 plt.setp(labels, rotation=90)
 plt.ylabel('Number of Paths that Contain each Country')
-#plt.title('Countries that Transit Tromboning ' + sys.argv[2] + ' Traffic')
-
-''' 
-This will put a label on top of the bar - put the number of unique IP addresses that geolocate to the country there.
-def autolabel(rects):
-    # attach some text labels
-    for rect in rects:
-        height = rect.get_height()
-        ax.text(rect.get_x() + rect.get_width()/2., 1.05*height,
-                '%d' % int(height),
-                ha='center', va='bottom')
-
-autolabel(rects1)
-autolabel(rects2)
-'''
 
 plt.savefig('tex_demo')
 plt.show()
